0714f27bd5c6edf330a9f83704a9385da1715755
leonredman <leonproart@yahoo.com>
2024-09-13 08:45:56 -0700

    git init useful snippets

---
+++ Practice-Labs/2.1-Practice-Lab-1/Python-Code-Snippets/list-input-example.py
@@ -0,0 +1,16 @@
+def get_grades():
+  grades = []
+
+  # Loop to get 4 grades from the user as a float and stored in list grades
+  for i in range(4):
+    grade = float(input(f"Enter grade {i + 1}: "))
+    grades.append(grade)
+
+  return grades
+
+if __name__ == '__main__':
+  #Get the list of grades
+  grades = get_grades()
+
+  # Display the grades as a list after 4 grades entered
+  print("The grades you entered are:", grades)
