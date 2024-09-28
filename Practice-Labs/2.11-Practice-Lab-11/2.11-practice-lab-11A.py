34f3687afebecb88b22ae61b7495865dc03c0b4e
leonredman <leonproart@yahoo.com>
2024-09-27 22:46:42 -0700

    lab solutions

---
+++ Practice-Labs/2.11-Practice-Lab-11/2.11-practice-lab-11A.py
@@ -0,0 +1,13 @@
+#check if the input range is between 1 and 10 for the range validation check
+
+if __name__ == '__main__': 
+        
+    r = range(1,10)
+    
+    num = int(input())
+    
+    # create conditional statement for range check here
+    if num in r:
+        print("The number input is in the range from 1 and 10.")
+    else:
+        print("The number input is not in the range from 1 and 10.")
